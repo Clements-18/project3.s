301fdcc2a7b1fb63f752b433f3694f16806ba9c6
Clements-18 <47756963+Clements-18@users.noreply.github.com>
2019-11-29 01:19:15 -0500

    commit
    see comments

--- project3.s
+++ project3.s
@@ -80,10 +80,31 @@ invalidloop:
 	beq $s0, 44, insubstring #check if bit is a comma
 	
 	
-	j invalidloop #jumps to the beginning of loop
+	j invalidloop              #jumps to the beginning of loop
 
 substring:
 	mul $t6,$t6,$t7 #if there was a space before a this valid character it will change $t6 to a positive num
+	
+	
+insubstring:
+	
+	addi $t1,$t1,1            #keeps track of the amount substring 	
+	
+	sub $sp, $sp,4            # creates space in the stack
+	
+	sw $t7, 0($sp)            #stores what was in $t5 into the stack
+	
+	move $t5,$t0 		  # store the pointer to the bit after the comma
+	lb $s0, ($t0)		  # loads the bit that $t0 is pointing to
+	beq $s0, 0, con1 	  #check if the bit is null
+	beq $s0, 10, con1 	 #checks if the bit is a new line 
+	beq $s0,44, invalidloop  #checks if the next bit is a comma
+	li $t3,0		 #resets the amount of valid characters back to 0
+	li $t6,0  		#resets my space/tabs checker back to 0
+	
+	
+	j start
+	
 next:
 	bgt $t6,0,insubstring      #checks to see if there were any spaces or tabs in between valid characters
 	bge $t3,5,insubstring      #checks to see if there are more than 4 for characters
@@ -95,6 +116,14 @@ next:
 	li $s1,0 		   #sets $s1 to 0 
 	
 
+
+jal SubgramB
+	lb $s0, ($t0) 
+	beq $s0, 0, con1 # check if the bit is null
+	beq $s0, 10, con1 #checks if the bit is a new line 
+	beq $s0,44, invalidloop #checks if the next bit is a comma
+	li $t6,0 #resets my space/tabs checker back to 0
+	j start
 
 
 
